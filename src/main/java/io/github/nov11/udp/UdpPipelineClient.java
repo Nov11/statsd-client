29a11a61e405998fd826ba4ab60838b84eb73a31
Nov11 <529079634@qq.com>
2018-12-10 00:41:31 +0800

    * should not use static fields. or it limit the usage in test

--- src/main/java/io/github/nov11/udp/UdpPipelineClient.java
+++ src/main/java/io/github/nov11/udp/UdpPipelineClient.java
@@ -1,21 +1,17 @@
 package io.github.nov11.udp;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.timeout.IdleStateHandler;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class UdpPipelineClient extends UdpClient {
-    private static final List<ChannelHandler> handlers = Arrays.asList(
-            new StringEncoder(StandardCharsets.UTF_8),
-            new IdleStateHandler(0, 1, 0, TimeUnit.SECONDS),
-            new MetricAggregationHandler());
-
     public UdpPipelineClient(String host, int port) {
-        super(host, port, handlers);
+        super(host, port, Arrays.asList(
+                new StringEncoder(StandardCharsets.UTF_8),
+                new IdleStateHandler(0, 1, 0, TimeUnit.SECONDS),
+                new MetricAggregationHandler()));
     }
 }
